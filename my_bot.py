4d8e33187b5eb8b6598f78d698c21324d4583d5f
owls4all <owls4all@gmail.com>
2025-01-27 12:06:37 -0800

    implemented a couple of functions

--- my_bot.py
+++ my_bot.py
@@ -1,7 +1,8 @@
-state = ""
-
-"""
-**Do NOT change the name of this function.**
+import random
+gameState = "not playing"
+choice = "none"
+fairchoice = 'none'
+"""**Do NOT change the name of this function.**
 
 This function will be called every time anyone says anything on a channel where the bot lives.
 
@@ -10,9 +11,14 @@ This function will be called every time anyone says anything on a channel where 
 * You can have the bot respond differently to different users
 """
 def should_i_respond(user_message, user_name):
-  global state
+  global gameState
   if "Frost" in user_message:
     return True
+  elif gameState != 'not playing':
+    if user_message in"RockrockPaperpaperScissorsscissors":
+      return True
+    else:
+      gameState = "cheat"
   else:
     return False
 
@@ -26,6 +32,57 @@ This function will be called every time the `should_i_respond` function returns 
 * You can have the bot respond differently to different messages and users
 """
 def respond(user_message, user_name):
-  global state
+  global gameState
+  global playerchoice
+  global fairchoice
   if user_message == 'Frost':
     return "That's me!"
+  if "what can you do" in user_message:
+    return """I can do lots of things! For instance I can...
+    - give a number between 1 and 10
+    - play rock paper scissors
+    - be annoying
+    - and so much more! [aka coming soon]
+    """
+  if "let's play rock paper scissors" in user_message or "lets play rock paper scissors" in user_message:
+    if gameState == 'not playing':
+      gameState == 'fair'
+      fairchoice = ['rock','paper','scissors']
+    return "All right. I've chosen something. What will you choose?"
+  if gameState == 'cheat':
+    if user_message.lowercase() == 'rock':
+      gameState = 'not playing'
+      return "I choose paper"
+    if user_message.lowercase() == 'paper':
+      gameState = 'not playing'
+      return "I choose scissors"
+    if user_message.lowercase() == 'scissors':
+      gameState = 'not playing'
+      return "rock"
+  if gameState == 'player wins':
+    if user_message.lowercase() == 'scissors':
+      gameState = 'not playing'
+      return "I choose paper"
+    if user_message.lowercase() == 'rock':
+      gameState = 'not playing'
+      return "I choose scissors"
+    if user_message.lowercase() == 'paper':
+      gameState = 'not playing'
+      return "rock"
+  if gameState == 'fair':
+    return f"I choose {fairchoice[random.randint(0,2)]}"
+  return "I don't know what that means"
+
+
+# For testing purposes:
+'''
+username = 'user'
+quit = False
+while not quit:
+  userInput = input('>>>')
+  if 'quit' in userInput:
+    quit=True
+  if should_i_respond(userInput,username):
+    print(respond(userInput,username))
+  
+'''
